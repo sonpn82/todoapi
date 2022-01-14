2ce43a85da968413e6c32be8e4f162621f181805
sonpn82 <pham_ngoc_son@yahoo.com>
2022-01-14 09:47:24 +0900

    commit on 'master'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 8080,
     bodyParser = require('body-parser');
     
 var todoRoutes = require("./routes/todos");
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 app.use('/api/todos', todoRoutes);
 
 app.listen(port, function(){
-    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
+    console.log("APP IS RUNNING ON PORT " + port);
 })
     
     
